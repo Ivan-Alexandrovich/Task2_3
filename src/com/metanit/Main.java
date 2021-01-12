c28f8dac3907cd2cd3b1eb4add52015186c83e4f
Ivan-Alexandrovich <77282266+Ivan-Alexandrovich@users.noreply.github.com>
2021-01-12 11:21:32 +0300

    Update Main.java

--- src/com/metanit/Main.java
+++ src/com/metanit/Main.java
@@ -1,5 +1,6 @@
 package com.metanit;
-
+/*Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, 
+и числа меньшие 6 умножить на 2;*/
 import java.util.Scanner;
 
 public class Main {
